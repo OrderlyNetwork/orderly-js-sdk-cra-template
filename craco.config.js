aec262f5024a4d40d3c5415587d0d223de4dd3f1
Jame Zhan <jame@orderly.network>
2024-11-27 20:51:05 +0800

    add node-polyfill-webpack-plugin

--- craco.config.js
+++ craco.config.js
@@ -1,40 +1,23 @@
-const webpack = require("webpack");
-const path = require("path");
-// const { GitRevisionPlugin } = require("git-revision-webpack-plugin");
+const NodePolyfillPlugin = require("node-polyfill-webpack-plugin");
 
 module.exports = {
   typescript: {
     enableTypeChecking: false,
   },
   webpack: {
-    configure: (webpackConfig, { env, paths }) => {
-      webpackConfig.resolve.fallback = {
-        https: require.resolve("https-browserify"),
-        crypto: require.resolve("crypto-browserify"),
-        os: require.resolve("os-browserify"),
-        http: require.resolve("stream-http"),
-        url: require.resolve("url"),
-        stream: require.resolve("stream-browserify"),
-        vm: require.resolve("vm-browserify"),
-        assert: require.resolve("assert"),
-        buffer: require.resolve("buffer"),
-        "process/browser": require.resolve("process/browser"),
-        zlib: require.resolve("browserify-zlib"),
-      };
-      return webpackConfig;
-    },
-    // alias: {
-    //     'bn.js': path.resolve(process.cwd(), 'node_modules', 'bn.js'),
-    //     goober: path.resolve(process.cwd(), 'node_modules', 'goober'),
+    // configure: (webpackConfig, { env, paths }) => {
+    //   return webpackConfig;
     // },
     plugins: [
-      new webpack.ProvidePlugin({
-        process: "process/browser",
+      /**
+       * plugin docs: https://github.com/Richienb/node-polyfill-webpack-plugin
+       * node stdlib browser docs: https://github.com/niksy/node-stdlib-browser
+       * If you need to reduce the bundle size, you can precisely specify the following polyfill:
+       * ["assert", "buffer", "crypto", "http", "https", "os", "process", "stream", "url", "vm", "zlib"]
+       */
+      new NodePolyfillPlugin({
+        additionalAliases: ["process"],
       }),
-      new webpack.ProvidePlugin({
-        Buffer: ["buffer", "Buffer"],
-      }),
-      // new webpack.DefinePlugin({ COMMIT_HASH }),
     ],
   },
 };
